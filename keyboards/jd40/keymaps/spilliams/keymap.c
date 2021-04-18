35be55e117071aa06a778133f0b1af5198b6f953
Spencer Williams <s@spencerenglish.com>
2021-04-18 15:33:55 -0700

    a keyboard should have a way to Delete

--- keyboards/jd40/keymaps/spilliams/keymap.c
+++ keyboards/jd40/keymaps/spilliams/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_GRV,  _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______,
         KC_TRNS, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL,  _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     // symbol layer
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,
         _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS,
-        _______, _______, _______, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______, KC_TRNS, KC_DEL,  _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     
     // Media layer
