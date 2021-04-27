c5eb10a7db9cca86cff59b6a398e7ed9364001ce
Spencer Williams <s@spencerenglish.com>
2021-04-26 21:44:40 -0700

    os controls should be distinct from keyboard controls

--- keyboards/jd40/keymaps/spilliams/keymap.c
+++ keyboards/jd40/keymaps/spilliams/keymap.c
@@ -4,8 +4,8 @@
 #define _NL 1
 #define _SL 2
 #define _FL 3
-#define _ML 4
-#define _CL 5
+#define _OL 4
+#define _KL 5
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // base layer
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    _______,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MO(_NL),
-        KC_LCTL, KC_LGUI, KC_LALT, MO(_NL), KC_BSPC, KC_SPC,  MO(_FL), MO(_CL), MO(_ML), MO(_SL)
+        KC_LCTL, KC_LGUI, MO(_NL), KC_LALT, KC_BSPC, KC_SPC,  MO(_FL), MO(_KL), MO(_OL), MO(_SL)
     ),
 
     // number layer
@@ -40,17 +40,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, KC_TRNS, KC_DEL,  _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     
-    // Media layer
-    [_ML] = LAYOUT(
-        _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_MPLY, _______, _______, _______,
+    // OS layer
+    [_OL] = LAYOUT(
+        KC_SLEP, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_MPLY, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS,
         _______, _______, _______, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     
-    // Control layer
-    [_CL] = LAYOUT(
-        KC_SLEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    // Keyboard layer
+    [_KL] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS,
         RESET,   _______, _______, KC_TRNS, _______, _______, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
