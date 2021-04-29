3385d7f1e7bd72543188ab453275b6c8ff92829d
Spencer Williams <s@spencerenglish.com>
2021-04-29 16:12:43 -0700

    i want to backspace while typing numbers

--- keyboards/tg4x/keymaps/spilliams/keymap.c
+++ keyboards/tg4x/keymaps/spilliams/keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
         KC_GRV,  _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS,
-        _______, _______, _______,        KC_DEL,                _______,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______,        KC_BSPC,               _______,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     // symbol layer
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_TRNS,
-        _______, _______, _______,        KC_DEL,                _______,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        _______, _______, _______,        KC_DEL,                KC_ENT,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     // os layer
