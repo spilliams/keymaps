01485895496091c919b57862072e8dadf04d461d
Spencer Williams <s@spencerenglish.com>
2021-05-07 11:35:14 -0700

    layer key can doubleduty as os key

--- keyboards/daisy/keymaps/spilliams/keymap.c
+++ keyboards/daisy/keymaps/spilliams/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  MO(_NL),
-        KC_LCTL, KC_LALT, MO(_NL),       KC_BSPC,           KC_SPC,             MO(_FL), MO(_CL), MO(_SL)
+        KC_LCTL, KC_LALT, LT(_NL, KC_LGUI), KC_BSPC,           KC_SPC,             MO(_FL), MO(_CL), MO(_SL)
     ),
     
     // number layer
