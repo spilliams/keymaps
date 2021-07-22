7270761e516b0c6d5d88626c3b80507210885b1f
Spencer Williams <s@spencerenglish.com>
2021-07-21 17:41:53 -0700

    tweak

--- keyboards/sofle/keymaps/spilliams/keymap.c
+++ keyboards/sofle/keymaps/spilliams/keymap.c
@@ -2,7 +2,7 @@
 
 enum sofle_layers {
     BASE,
-    SYMB,
+    SUPE,
     HYPE,
 };
 
@@ -19,17 +19,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Play  |    | Mute  |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *            | LCtl | LAlt | SYMB | Bspc | /  Del  /       \ Ret  \  | Spc  | SYMB | GUI  | RCtl |
+ *            | LCtl | LAlt | SUPE | Bspc | /  Del  /       \ Ret  \  | Spc  | SUPE | GUI  | RCtl |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
  *            `----------------------------------'           '------''---------------------------'
  */
 
 [BASE] = LAYOUT( \
-    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_LBRC, \
+    KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_MINS, \
     KC_GRV,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_BSLS, \
     KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  KC_QUOT, \
     KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MPLY,   KC_MUTE, KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT, \
-                KC_LCTL,  KC_LALT,  MO(SYMB), KC_BSPC,  KC_DEL,    KC_ENT,   KC_SPC, MO(SYMB), KC_LGUI,  KC_RCTL \
+                KC_LCTL,  KC_LALT,  MO(SUPE), KC_BSPC,  KC_DEL,    KC_ENT,   KC_SPC, MO(SUPE), KC_LGUI,  KC_RCTL \
 ),
 /*
  * Symbols
@@ -47,8 +47,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 
-[SYMB] = LAYOUT( \
-    KC_TRNS, KC_NO,  KC_NO,  KC_NO,   KC_NO,    KC_EQL,                   KC_MINS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, \
+[SUPE] = LAYOUT( \
+    KC_TRNS, KC_NO,  KC_NO,  KC_NO,   KC_NO,    KC_EQL,                   KC_MINS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_EQL, \
     KC_TRNS, KC_NO,  KC_NO,  KC_LCBR, KC_RCBR,  KC_NO,                      KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO, \
     KC_TRNS, KC_NO,  KC_NO,  KC_LPRN, KC_RPRN,  KC_NO,                      KC_LEFT,KC_DOWN,KC_UP,KC_RIGHT, KC_NO,  KC_NO, \
     KC_TRNS, KC_NO,  KC_NO,  KC_LBRC, KC_RBRC,  KC_NO, KC_TRNS,   KC_TRNS,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_TRNS, \
