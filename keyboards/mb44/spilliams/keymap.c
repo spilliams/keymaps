62fc8799a0112132ddbeb03b09ce3b2e67cc61b8
Spencer Williams <s@spencerenglish.com>
2021-11-11 14:18:58 -0800

    tweak

--- keyboards/mb44/spilliams/keymap.c
+++ keyboards/mb44/spilliams/keymap.c
@@ -10,23 +10,23 @@ enum layer_names {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_2u1u_space(
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSLS,
+        KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_QUOT, KC_BSLS,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_SLSH,
-        KC_LCTL, KC_LALT, MO(_NL), KC_BSPC,    KC_ENT,  KC_SPC,                          KC_LEFT, KC_DOWN, KC_RGHT
+        MO(_NL), KC_LCTL, KC_LALT, KC_BSPC,    KC_ENT,  KC_SPC,                          KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_NL] = LAYOUT_2u1u_space(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
-        KC_GRV,  XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_QUOT, XXXXXXX,
+        KC_GRV,  XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SCLN, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PGUP, MO(_FL),
-        _______, _______, KC_TRNS, KC_DEL,     KC_NO,   KC_ENT,                          KC_HOME, KC_PGDN, KC_END
+        KC_TRNS, _______, _______, KC_DEL,     KC_NO,   KC_ENT,                          KC_HOME, KC_PGDN, KC_END
     ),
 
     [_FL] = LAYOUT_2u1u_space(
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_6,    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, RESET,   XXXXXXX, XXXXXXX, KC_MUTE, KC_MPLY, KC_VOLU, KC_TRNS,
-        _______, _______, KC_TRNS, _______,    KC_MUTE, _______,                         KC_MPRV, KC_VOLD, KC_MNXT
+        KC_TRNS, _______, _______, _______,    KC_MUTE, _______,                         KC_MPRV, KC_VOLD, KC_MNXT
     ),
 };
 
